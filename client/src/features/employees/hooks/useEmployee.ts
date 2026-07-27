735eb894fc4204431d6078a930fb2a7a149a3c1f
Atilola Victor <atilolavictor4562@gmail.com>
2026-07-27 08:44:53 -0700

    refactor(employees): update employee hooks for mutations and filters

--- client/src/features/employees/hooks/useEmployee.ts
+++ client/src/features/employees/hooks/useEmployee.ts
@@ -3,7 +3,7 @@ import { getEmployees } from "../api/employees.api"
 
 export const useEmployee= ()=> { 
   return useQuery({
-    queryKey: ['employees-list'],
+    queryKey: ['employees'],
     queryFn: getEmployees,
      staleTime: 1000 * 60 * 5
   })
