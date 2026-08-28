f7e8c777da1b6e5495ad8dd273e43198af63e430
Atilola Victor <atilolavictor4562@gmail.com>
2026-08-28 06:48:10 -0700

    feat(employees): update managers hook

--- client/src/features/employees/hooks/useManagers.ts
+++ client/src/features/employees/hooks/useManagers.ts
@@ -2,9 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import type { Manager } from "../types/employee.types";
 import { getManagers } from "../api/employees.api";
 
-export const useManagers = () => {
+export const useManagers = (options?: { enabled?: boolean }) => {
   return useQuery<Manager[]>({
     queryKey: ["managers"],
     queryFn: getManagers,
+    enabled: options?.enabled ?? true,
   });
 };
