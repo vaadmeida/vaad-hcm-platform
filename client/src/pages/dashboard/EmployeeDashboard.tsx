72da4fe532ac72c9648754e0e44fea5b2606b7b7
Atilola Victor <atilolavictor4562@gmail.com>
2026-08-28 06:45:51 -0700

    feat(dashboard): update employee dashboard layout

--- client/src/pages/dashboard/EmployeeDashboard.tsx
+++ client/src/pages/dashboard/EmployeeDashboard.tsx
@@ -1,3 +1,6 @@
+import EmployeeByDepartmentChart from "@/features/dashboard/components/EmployeeByDepartmentsChart";
+import EmployeeDashboardStats from "@/features/dashboard/components/EmployeeDashboardStats";
+import { useEmployeesByDepartment } from "@/features/dashboard/hooks/useEmployeesByDepartment";
 import { useAuthStore } from "@/store/auth.store";
 import { dashboardSubtitle } from "@/utils/dashboardSubtitle";
 import { getGreeting } from "@/utils/date"
@@ -6,6 +9,8 @@ const EmployeeDashboard = () => {
 
 
   const user = useAuthStore((state) => state.user);
+
+  const { data: employeeDepartments } = useEmployeesByDepartment();
   return (
     <div>
       <div className="space-y-8">
@@ -15,11 +20,23 @@ const EmployeeDashboard = () => {
             {getGreeting()}, {user?.first_name} 👋
           </h1>
 
-            <p className="mt-1 text-sm text-muted-foreground">
-              {dashboardSubtitle[user?.role ?? "employee"]}
-            </p>
-    
+          <p className="mt-1 text-sm text-muted-foreground">
+            {dashboardSubtitle[user?.role ?? "employee"]}
+          </p>
+
         </section>
+
+        {/* Stats Cards */}
+        <EmployeeDashboardStats />,
+
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+          <EmployeeByDepartmentChart
+            data={employeeDepartments?.data ?? []}
+          />
+
+
+        </div>
+
       </div>
     </div>
   )
