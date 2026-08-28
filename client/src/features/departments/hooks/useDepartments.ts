0e8e8994f6a367eeecb699b6309b042004d16abd
Atilola Victor <atilolavictor4562@gmail.com>
2026-08-28 06:48:21 -0700

    feat(departments): update departments hook

--- client/src/features/departments/hooks/useDepartments.ts
+++ client/src/features/departments/hooks/useDepartments.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import type { Department } from "../types/departments.types";
 import { getDepartments } from "../api/department.api";
 
-export const useDepartments = () => {
+export const useDepartments = (options?: { enabled?: boolean }) => {
   return useQuery<Department[]>({
     queryKey: ["departments"],
     queryFn: getDepartments,
-    
+    enabled: options?.enabled ?? true,
   });
 };
