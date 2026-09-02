5ff9031d1b1989636af1f711248a7c46740960ab
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-02 08:46:53 -0700

    chore: update database seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -29,9 +29,40 @@ async function createAdmin() {
   console.log("Admin created:", admin.email);
 }
 
-createAdmin()
-  .catch((error) => {
+async function createOrganization() {
+  const organization = await prisma.organization.upsert({
+    where: {
+      id: "00000000-0000-0000-0000-000000000001",
+    },
+
+    update: {},
+
+    create: {
+      id: "00000000-0000-0000-0000-000000000001",
+      name: "VAAD MEDIA LTD",
+      industry: "Out-of-Home Advertising",
+      email: "",
+      company_size: "11-50",
+      phone: "",
+      website: "",
+      street_address: "",
+      city: "",
+      state: "",
+      country: "Nigeria",
+    },
+  });
+
+  console.log("Organization created:", organization.name);
+}
+
+async function main() {
+  await createAdmin();
+  await createOrganization();
+}
+
+main().catch((error) => {
     console.error(error);
+    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
