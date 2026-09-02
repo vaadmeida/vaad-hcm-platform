591c47a6b14aa269356d2ee81f9fb68705e23f8c
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-02 08:44:23 -0700

    refactor: update leave tabs

--- client/src/features/leaves/components/LeaveTabs.tsx
+++ client/src/features/leaves/components/LeaveTabs.tsx
@@ -1,7 +1,13 @@
+import { useAuthStore } from "@/store/auth.store";
 import { FileText, LayoutDashboard, Settings, Wallet } from "lucide-react";
 
-export type LeaveTabsTypes = 'Overview' | 'Requests' | 'Balances' | 'Leave Types' | 'My Leaves'
 
+export type LeaveTabsTypes =
+    | "Overview"
+    | "Requests"
+    | "Balances"
+    | "Leave Types"
+    | "My Leaves";
 
 interface LeaveTabsProps {
     activeTab: LeaveTabsTypes;
@@ -37,11 +43,14 @@ const tabs = [
 ];
 
 const LeaveTabs = ({ activeTab, onTabChange }: LeaveTabsProps) => {
+    const user = useAuthStore((state) => state.user);
+
+    const visibleTabs = tabs.filter((tab) => !(tab.id === "My Leaves" && user?.role === "admin"));
 
     return (
         <div className="mt-6 border-b border-gray-200">
             <div className="flex gap-6 overflow-x-auto">
-                {tabs.map((tab) => {
+                {visibleTabs.map((tab) => {
                     const Icon = tab.icon;
                     const isActive = activeTab === tab.id;
 
@@ -50,10 +59,11 @@ const LeaveTabs = ({ activeTab, onTabChange }: LeaveTabsProps) => {
                             key={tab.id}
                             type="button"
                             onClick={() => onTabChange(tab.id)}
-                            className={`relative flex shrink-0 items-center gap-2 pb-3 text-sm font-medium transition-colors cursor-pointer ${isActive
+                            className={`relative flex shrink-0 cursor-pointer items-center gap-2 pb-3 text-sm font-medium transition-colors ${
+                                isActive
                                     ? "text-primary"
                                     : "text-muted-foreground hover:text-foreground"
-                                }`}
+                            }`}
                         >
                             <Icon className="h-4 w-4" />
 
@@ -67,7 +77,7 @@ const LeaveTabs = ({ activeTab, onTabChange }: LeaveTabsProps) => {
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LeaveTabs
+export default LeaveTabs;
