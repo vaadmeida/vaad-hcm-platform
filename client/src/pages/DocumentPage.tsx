8b58a6b0a333f95c0060ce7ef99aebe2baf9c291
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-04 08:46:13 -0700

    feat: add document management page

--- client/src/pages/DocumentPage.tsx
+++ client/src/pages/DocumentPage.tsx
@@ -1,7 +1,33 @@
+import type { DocumentTabsTypes } from "@/features/documents/components/DocumentTabs"
+import DocumentTabs from "@/features/documents/components/DocumentTabs"
+import DocumentToolBar from "@/features/documents/components/DocumentToolBar"
+import Documents from "@/features/documents/components/section/documents/Documents"
+import DocumentTypes from "@/features/documents/components/section/documentsTypes/DocumentTypes"
+import DocumentOverview from "@/features/documents/components/section/overview/DocumentOverview"
+import { useState } from "react"
+
 const DocumentPage = () => {
+
+    const [activeTab, setActiveTab] = useState<DocumentTabsTypes>('Overview')
+
   return (
-    <div>
-         <h1>Document Page</h1>
+    <div className="space-y-6">
+      <DocumentToolBar />
+
+      <DocumentTabs
+       activeTab={activeTab}
+       onTabChange={setActiveTab}
+      />
+
+       {activeTab === 'Overview' && <DocumentOverview />}
+
+       {activeTab === 'Documents' && (<Documents />)}
+
+       {activeTab === 'Document Types' && (
+          <DocumentTypes />
+       )} 
+ 
+
     </div>
   )
 }
