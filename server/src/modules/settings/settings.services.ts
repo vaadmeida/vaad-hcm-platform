35e809520ae1899bb01f9aa07f6ec7e72945d299
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-04 01:51:08 -0700

    fix organization settings service

--- server/src/modules/settings/settings.services.ts
+++ server/src/modules/settings/settings.services.ts
@@ -112,6 +112,8 @@ export const UpdateMyProfile = async (
 export const getOrganization = async () => {
     const organization = await prisma.organization.findFirst();
 
+    console.log("PRODUCTION ORGANIZATION:", organization);
+
     if (!organization) {
         throw new AppError(
             "Organization not found",
