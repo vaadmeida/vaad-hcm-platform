fa8be6cb4fede6a5f6cdbc4e917dcb24996b324a
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-07 07:49:05 -0700

    feat: update auth controller

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
-import { loginUser, refreshAccessToken, } from './auth.services.ts';
+import { changePassword, loginUser, refreshAccessToken, } from './auth.services.ts';
 import { AppError } from '../../errors/appError.ts';
+import { changePasswordValidator } from './auth.validator.ts';
 
 type LoginRequestBody = {
   email: string;
@@ -12,7 +13,7 @@ export const loginUserController = async (req: Request<{}, {}, LoginRequestBody>
   try {
     const { email, password } = req.body;
 
-    const {  data } = await loginUser(email, password);
+    const { data } = await loginUser(email, password);
 
     res.status(200).json({
       "status": "200",
@@ -54,10 +55,53 @@ export const refreshAccessTokenController = async (req: Request, res: Response) 
 
   } catch (error) {
     res.status(403).json({
-        status: 403,
+      status: 403,
       code: "SERVER_ERROR",
       message: "Internal server error",
       detail: null,
     });
   }
 };
+
+
+
+export const changePasswordController = async (req: Request, res: Response) => {
+
+  const parsed = changePasswordValidator.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      status: 400,
+      code: "VALIDATION_ERROR",
+      message: "Invalid request data",
+      detail: parsed.error.format(),
+    });
+  }
+
+  const { current_password, new_password } = parsed.data;
+  console.log("AUTH USER:", req.user);
+
+  const userId = req.user?.id;
+
+  console.log("USER ID:", userId);
+  if (!userId) {
+    return res.status(401).json({
+      status: 401,
+      code: "UNAUTHORIZED",
+      message: "Unauthorized",
+      detail: null,
+    });
+  }
+
+
+
+  await changePassword(userId, current_password, new_password);
+
+  res.status(200).json({
+    status: 200,
+    code: "PASSWORD_CHANGED",
+    message: "Password changed successfully",
+    detail: null,
+  });
+
+}
