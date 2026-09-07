7ee4e18b090bfd53f4d2ab2044ad8afab45f09d7
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-07 07:48:20 -0700

    feat: update auth api

--- client/src/features/auth/api/auth.api.ts
+++ client/src/features/auth/api/auth.api.ts
@@ -1,10 +1,16 @@
 import type { LoginFormData } from "@/features/auth/validations/login.schema"
 import { api } from "../../../lib/axios.api"
-import type { LoginResponse } from "@/features/auth/types/auth.types"
+import type { ChangePasswordPayload, LoginResponse } from "@/features/auth/types/auth.types"
 
 export const login = async(data: LoginFormData): Promise<LoginResponse> =>{
  
-    const response  = await api.post('/api/auth/login', data)
+    const response  = await api.post<LoginResponse>('/api/auth/login', data)
     return response.data
-     
 }
+
+
+export const changePassword = async ( data: ChangePasswordPayload) => {
+  const response = await api.post("/api/auth/change-password", data );
+
+  return response.data;
+};
