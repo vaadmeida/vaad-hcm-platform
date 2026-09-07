d53c967d245fc534c3c258a94dde490b5ade3d74
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-07 03:32:47 -0700

    refactor(leaves): replace upcoming leaves with current leaves

--- client/src/features/leaves/components/section/overview/LeaveRequestsByType.tsx
+++ client/src/features/leaves/components/section/overview/LeaveRequestsByType.tsx
@@ -1,10 +1,11 @@
+import CurrentlyOnLeaves from "./CurrentlyOnLeaves"
 import RecentLeavesRequest from "./RecentLeavesRequest"
-import UpcomingApprovedLeaves from "./UpcomingApprovedLeaves"
+
 
 const LeaveRequestsByType = () => {
     return (
         <div className="grid items-start gap-6 lg:grid-cols-2 mt-8">
-            <UpcomingApprovedLeaves />
+            <CurrentlyOnLeaves/>
             <RecentLeavesRequest />
         </div>
     )
--- client/src/features/leaves/hooks/useUpcomingLeaves.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUpcomingLeavesRequests } from "../api/leave.api";
-
-export const useUpcomingLeaves = () => {
-  return useQuery({
-    queryKey: ["upcoming-leave-requests"],
-    queryFn: getUpcomingLeavesRequests,
-  });
-};
