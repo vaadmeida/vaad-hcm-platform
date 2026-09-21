ff294e284e5a865f3d692a70945ec68600735e82
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-21 08:59:09 -0700

    feat(documents): add document types query hook

---
+++ client/src/features/documents/hooks/useDocumentsTypes.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getDocumentTypes } from "../api/documents.api";
+
+export const useDocumentTypes = () => {
+    return useQuery({
+        queryKey: ["document-types"],
+        queryFn: getDocumentTypes,
+    });
+};
