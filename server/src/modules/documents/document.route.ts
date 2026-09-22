2baf90e69088b2770cd242217cf4b11cccc25f28
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-22 08:43:31 -0700

    feat: update document routes

--- server/src/modules/documents/document.route.ts
+++ server/src/modules/documents/document.route.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import { authenticate } from '../../middlewares/auth.ts'
-import { createDocumentTypeController, getAllEmployeeDocumentsController, getDocumentByIdController, getDocumentDownloadUrlController, getDocumentsStatsController, getDocumentTypesController, getEmployeeDocumentsController, getExpiredDocumentsController, getExpiringDocumentsController, getRecentDocumentsController, selfUploadDocumentController, uploadEmployeeDocumentController, verifyDocumentController } from './document.controller.ts';
+import { createDocumentTypeController, getAllEmployeeDocumentsController, getDocumentByIdController, getDocumentDownloadUrlController, getDocumentsStatsController, getDocumentTypesController, getEmployeeDocumentsController, getExpiredDocumentsController, getExpiringDocumentsController, getMyDocumentsController, getMyDocumentStatsController, getRecentDocumentsController, selfUploadDocumentController, uploadEmployeeDocumentController, verifyDocumentController } from './document.controller.ts';
 import { asyncHandler } from '../../utils/asyncHandler.ts';
 import { requireRoles } from '../../middlewares/role.ts';
 import upload from '../../middlewares/upload.ts';
 
 const documentRouter = express.Router();
 
-documentRouter.post('/',authenticate, requireRoles('admin'), asyncHandler(createDocumentTypeController))
+documentRouter.post('/',authenticate, requireRoles('admin', "hr"), asyncHandler(createDocumentTypeController))
 documentRouter.get('/', authenticate, requireRoles('admin', 'hr'), asyncHandler(getAllEmployeeDocumentsController))
-documentRouter.get('/types', authenticate, requireRoles('admin', 'hr'), asyncHandler(getDocumentTypesController))
+documentRouter.get('/types', authenticate , asyncHandler(getDocumentTypesController))
 documentRouter.get('/stats', authenticate, requireRoles('admin', 'hr'), asyncHandler(getDocumentsStatsController))
+documentRouter.get('/my/stats', authenticate, asyncHandler(getMyDocumentStatsController))
+documentRouter.get('/my', authenticate, asyncHandler(getMyDocumentsController))
 documentRouter.get('/recent', authenticate, requireRoles('admin', 'hr'), asyncHandler(getRecentDocumentsController))
 documentRouter.get('/expiring', authenticate, requireRoles('admin', 'hr'), asyncHandler(getExpiringDocumentsController))
 documentRouter.get('/expired', authenticate, requireRoles('admin', 'hr'), asyncHandler(getExpiredDocumentsController))
 documentRouter.post('/me', authenticate, upload.single('file'), asyncHandler(selfUploadDocumentController))
-documentRouter.post('/employee/:employeeId', authenticate, requireRoles("admin", "hr"), upload.single('file'), asyncHandler(uploadEmployeeDocumentController))
+documentRouter.post('/employee/:employeeId', authenticate, upload.single('file'), asyncHandler(uploadEmployeeDocumentController))
 documentRouter.get('/employee/:employeeId', authenticate, asyncHandler(getEmployeeDocumentsController))
 documentRouter.get('/:documentId', authenticate, asyncHandler(getDocumentByIdController))
 documentRouter.get('/download/:documentId', authenticate, asyncHandler(getDocumentDownloadUrlController))
